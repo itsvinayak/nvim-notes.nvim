b0226cf31e494d062aa38943facfee10da4391be
vinayak <itssvinayak@gmail.com>
2024-03-24 23:18:21 +0530

    fix: removed git usage

--- lua/notes.lua
+++ lua/notes.lua
@@ -7,14 +7,8 @@ local function get_notes_path()
 	return path
 end
 
--- Function to prompt the user to input the Git URL for saving notes
-local function get_git_url_to_save_notes()
-	local git_url = vim.fn.input("Enter the git url to save notes: ")
-	return git_url
-end
-
--- Function to create a folder for saving notes and initialize Git repository
-function notes.create_folder_to_save_notes(git_url)
+-- Function to create a folder for saving notes
+function notes.create_folder_to_save_notes()
 	-- Get the notes directory path
 	local path = get_notes_path()
 	-- Create the directory if it doesn't exist
@@ -24,21 +18,6 @@ function notes.create_folder_to_save_notes(git_url)
 		print("Notes directory already exists.")
 		return
 	end
-
-	-- Prompt user for Git URL if not provided
-	local git_url = git_url or get_git_url_to_save_notes()
-	if git_url == "" then
-		print("Skipping saving to git.")
-		return
-	end
-
-	-- Change directory to the notes folder and initialize Git
-	os.execute("cd " .. path)
-	os.execute("git init")
-	os.execute("git remote add origin " .. git_url)
-	os.execute("git pull origin master --allow-unrelated-histories")
-	-- Change back to the original directory
-	os.execute("cd -")
 end
 
 function notes.is_setup_required()
@@ -51,10 +30,7 @@ end
 
 -- Function to set up notes environment
 function notes.setup()
-	-- Get the git URL and path from the arguments
-	local git_url = vim.fn.input("Enter the git url to save notes: ")
-	print("Git URL: ", git_url)
-	notes.create_folder_to_save_notes(git_url)
+	notes.create_folder_to_save_notes()
 	vim.g.notes_loaded = true
 end
 
@@ -83,13 +59,6 @@ function notes.write_notes()
 	local filename = path .. "/note_" .. os.date("%m-%d-%H-%M-%S") .. ".txt"
 	-- Open a new file for writing notes
 	vim.cmd("edit " .. filename)
-	vim.api.nvim_command([[
-	    autocmd BufWritePost * lua
-		local filename = vim.fn.expand("<afile>")
-		os.execute("git add " .. filename)
-		os.execute("git commit -m 'Add note on " .. os.date("%m-%d-%H-%M-%S") .. "'")
-		os.execute("git push --all")
-	]])
 end
 
 return notes
