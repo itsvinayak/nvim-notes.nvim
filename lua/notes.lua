6df716dbad06f39f57b6dbb361c41ea333f2c81a
vinayak <itssvinayak@gmail.com>
2024-03-26 07:45:33 +0530

    fix: format fixes

--- lua/notes.lua
+++ lua/notes.lua
@@ -2,53 +2,53 @@ local notes = {}
 
 -- Function to get the path where notes will be saved
 local function get_notes_path()
-	local home = os.getenv("HOME")
-	local path = home .. "/.notes"
-	return path
+  local home = os.getenv 'HOME'
+  local path = home .. '/.notes'
+  return path
 end
 
 -- Function to create a folder for saving notes
 function notes.create_folder_to_save_notes()
-	-- Get the notes directory path
-	local path = get_notes_path()
-	-- Create the directory if it doesn't exist
-	if vim.fn.isdirectory(path) == 0 then
-		print("Creating directory : ", path)
-		vim.fn.mkdir(path, "p")
-	end
+  -- Get the notes directory path
+  local path = get_notes_path()
+  -- Create the directory if it doesn't exist
+  if vim.fn.isdirectory(path) == 0 then
+    print('Creating directory : ', path)
+    vim.fn.mkdir(path, 'p')
+  end
 end
 
 function notes.setup()
-	-- Create a folder to save notes
-	notes.create_folder_to_save_notes()
+  -- Create a folder to save notes
+  notes.create_folder_to_save_notes()
 end
 
 -- Function to search for notes using Telescope plugin
 function notes.get_notes()
-	local path = get_notes_path()
-	require("telescope.builtin").find_files({
-		prompt_title = "Get Notes",
-		cwd = path,
-	})
+  local path = get_notes_path()
+  require('telescope.builtin').find_files {
+    prompt_title = 'Get Notes',
+    cwd = path,
+  }
 end
 
 -- Function to search within notes using Telescope plugin
 function notes.find_notes()
-	local path = get_notes_path()
-	require("telescope.builtin").live_grep({
-		prompt_title = "Find Notes",
-		cwd = path,
-	})
+  local path = get_notes_path()
+  require('telescope.builtin').live_grep {
+    prompt_title = 'Find Notes',
+    cwd = path,
+  }
 end
 
 -- Function to write new notes
 function notes.write_notes()
-	local path = get_notes_path()
-	-- Generate a filename with current date and time
-	local filename = path .. "/note_" .. os.date("%m-%d-%H-%M-%S") .. ".txt"
-	notes.create_folder_to_save_notes()
-	-- Open a new file for writing notes
-	vim.cmd("edit " .. filename)
+  local path = get_notes_path()
+  -- Generate a filename with current date and time
+  local filename = path .. '/note_' .. os.date '%m-%d-%H-%M-%S' .. '.txt'
+  notes.create_folder_to_save_notes()
+  -- Open a new file for writing notes
+  vim.cmd('edit ' .. filename)
 end
 
 return notes
--- plugin/notes.lua
+++ plugin/notes.lua
@@ -1,47 +1,47 @@
 if vim.g.notes_loaded then
-	return -- If loaded, exit to prevent reloading
+  return -- If loaded, exit to prevent reloading
 end
 
 -- Mark notes plugin as loaded
 vim.g.notes_loaded = true
 
 -- Create a user command named "Notes" that can be used to interact with the notes plugin
-vim.api.nvim_create_user_command("Notes", function(opts)
-	local args = opts.fargs
-	-- Check if any arguments were provided
-	if #args == 0 then
-		-- Print error message if no arguments were provided
-		print("Invalid command. Choose from write, find or get")
-		return
-	end
-	-- Check the first argument to determine the action
-	if args[1] == "write" then
-		-- Call the write_notes function from the notes module
-		require("notes").write_notes()
-	elseif args[1] == "find" then
-		-- Call the find_notes function from the notes module
-		require("notes").find_notes()
-	elseif args[1] == "get" then
-		-- Call the get_notes function from the notes module
-		require("notes").get_notes()
-	elseif args[1] == "setup" then
-		-- Call the setup function from the notes module
-		require("notes").setup()
-	elseif args[1] == "help" then
-		-- Print help message if help command was provided
-		print("Notes plugin commands:")
-		print("  :Notes write - Write a new note")
-		print("  :Notes find - Find notes by title")
-		print("  :Notes get - Get a list of all notes")
-		print("  :Notes setup - Setup the notes plugin")
-	else
-		-- Print error message if an invalid command was provided
-		print("Invalid command: " .. args[1] .. ". Choose from write, find or get")
-	end
-end, { nargs = "*" })
+vim.api.nvim_create_user_command('Notes', function(opts)
+  local args = opts.fargs
+  -- Check if any arguments were provided
+  if #args == 0 then
+    -- Print error message if no arguments were provided
+    print 'Invalid command. Choose from write, find or get'
+    return
+  end
+  -- Check the first argument to determine the action
+  if args[1] == 'write' then
+    -- Call the write_notes function from the notes module
+    require('notes').write_notes()
+  elseif args[1] == 'find' then
+    -- Call the find_notes function from the notes module
+    require('notes').find_notes()
+  elseif args[1] == 'get' then
+    -- Call the get_notes function from the notes module
+    require('notes').get_notes()
+  elseif args[1] == 'setup' then
+    -- Call the setup function from the notes module
+    require('notes').setup()
+  elseif args[1] == 'help' then
+    -- Print help message if help command was provided
+    print 'Notes plugin commands:'
+    print '  :Notes write - Write a new note'
+    print '  :Notes find - Find notes by title'
+    print '  :Notes get - Get a list of all notes'
+    print '  :Notes setup - Setup the notes plugin'
+  else
+    -- Print error message if an invalid command was provided
+    print('Invalid command: ' .. args[1] .. '. Choose from write, find or get')
+  end
+end, { nargs = '*' })
 
 -- Define key mappings for commonly used commands
-vim.api.nvim_set_keymap("n", "<leader>nw", ":Notes write<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>nf", ":Notes find<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>ng", ":Notes get<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>ns", ":Notes setup<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>nw', ':Notes write<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>nf', ':Notes find<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ng', ':Notes get<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ns', ':Notes setup<CR>', { noremap = true, silent = true })
