08fae7155b385b8104075136899dd16d40422715
vinayak <itssvinayak@gmail.com>
2024-10-06 03:21:41 +0530

    fix: path for notes

--- lua/notes.lua
+++ lua/notes.lua
@@ -1,65 +1,68 @@
 local utils = require 'utils'
 local notes = {}
 
--- Default configuration
+-- default configuration
 local default_config = {}
 default_config = {}
 default_config.path = utils.get_notes_path()
-default_config.log_file = os.getenv 'HOME' .. '/.config/nvim/log/notes.log'
-default_config.log_level = 'INFO'
+default_config.log_level = 'info'
 default_config.log_enabled = false
 
 notes.config = default_config
 
--- Function to setup the notes plugin
+-- function to setup the notes plugin
 function notes.setup(user_config)
   if pcall(require, 'telescope') == false then
-    utils.log('Telescope is not installed. Please install it to use notes plugin', 'ERROR', true)
+    utils.log('telescope is not installed. please install it to use notes plugin', 'error', true)
     return
   end
-  -- Merge user-provided config with the default configuration
+  -- merge user-provided config with the default configuration
   notes.config = utils.merge_config(default_config, user_config)
-  -- Create a folder to save notes
-  utils.log('Creating folder to save notes' .. notes.config.path, notes.config.log_level, notes.config.log_enabled)
+  -- create a folder to save notes
+  utils.log('creating folder to save notes' .. notes.config.path, notes.config.log_level, notes.config.log_enabled)
   notes.create_folder_to_save_notes()
 end
 
--- Function to create a folder for saving notes
+-- function to create a folder for saving notes
 function notes.create_folder_to_save_notes()
-  -- Get the notes directory path
-  local path = notes.config.path
-  -- Create the directory if it doesn't exist
+  -- get the notes directory path
+  local path = vim.fn.expand(notes.config.path)
+  -- create the directory if it doesn't exist
   if vim.fn.isdirectory(path) == 0 then
-    utils.log('Creating directory : ', path)
-    vim.fn.mkdir(path, 'p')
+    if not vim.fn.mkdir(path, 'p') then
+      vim.notify('failed to create directory for notes ' .. path)
+      utils.log('failed to create directory : ', path, 'error', true)
+    end
+    utils.log('created directory : ', path)
+    vim.notify('created directory for notes ' .. path)
   end
 end
 
--- Function to search for notes using Telescope plugin
+-- function to search for notes using telescope plugin
 function notes.get_notes()
   local path = notes.config.path
   require('telescope.builtin').find_files {
-    prompt_title = 'Get Notes',
+    prompt_title = 'get notes',
     cwd = path,
   }
 end
 
--- Function to search within notes using Telescope plugin
+-- function to search within notes using telescope plugin
 function notes.find_notes()
   local path = notes.config.path
   require('telescope.builtin').live_grep {
-    prompt_title = 'Find Notes',
+    prompt_title = 'find notes',
     cwd = path,
   }
 end
 
--- Function to write new notes
+-- function to write new notes
 function notes.write_notes()
   local path = notes.config.path
-  -- Generate a filename with current date and time
-  local filename = path .. '/note_' .. os.date '%m-%d-%H-%M-%S' .. '.txt'
+  -- generate a filename with current date and time
+  local filename = path .. '/note_' .. os.date '%m-%d-%h-%m-%s' .. '.txt'
   notes.create_folder_to_save_notes()
-  -- Open a new file for writing notes
+  -- open a new file for writing notes
   vim.cmd('edit ' .. filename)
 end
 
